ffa76f83efa938a0ec19a9f2017fa279768899c4
fakhri <fakhri.warnet@live.com>
2021-02-03 13:49:43 +0700

    master

---
+++ application/logs/log-2021-01-31.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-01-31 12:54:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\Ecommerce-CodeIgniter-Bootstrap\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-01-31 12:54:16 --> Unable to connect to the database
+ERROR - 2021-01-31 12:54:20 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\Ecommerce-CodeIgniter-Bootstrap\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-01-31 12:54:20 --> Unable to connect to the database
+ERROR - 2021-01-31 12:54:43 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:54:47 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:54:49 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:54:59 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:55:07 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:55:10 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:55:12 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:55:28 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:55:34 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:56:34 --> 404 Page Not Found: /index
+ERROR - 2021-01-31 12:58:20 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2021-01-31 12:58:34 --> 404 Page Not Found: ../modules/admin/controllers//index
